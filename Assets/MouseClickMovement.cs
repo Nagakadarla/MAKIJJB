803e12e9c0bebd912832198c5e958bee9ed14b32
Nagakadarla <144846507+Nagakadarla@users.noreply.github.com>
2023-10-04 20:18:23 -0400

    Add files via upload

---
+++ Assets/MouseClickMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    Vector2 lastClickedPos;
+    bool moving;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+        if (moving && (Vector2)transform.position != lastClickedPos)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos,
+            step);
+        }
+        else
+        {
+            moving = false;
+        }
+    }
+
+}
